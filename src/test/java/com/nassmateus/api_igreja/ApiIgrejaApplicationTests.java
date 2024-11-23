bd42a9502e5bad49abad387e7530e7d3b1e2b077
Mateus Nass <matenass@hotmail.com>
2024-11-22 22:37:44 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/nassmateus/api_igreja/ApiIgrejaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nassmateus.api_igreja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiIgrejaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
